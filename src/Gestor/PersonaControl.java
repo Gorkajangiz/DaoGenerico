2b0826f8f1e4637ed73ec0b6b8d9af9d9a89384b
gorka <gorka@GrokaPortable>
2025-05-12 15:45:48 +0200

    Segunda version

--- src/Gestor/PersonaControl.java
+++ src/Gestor/PersonaControl.java
@@ -4,12 +4,49 @@
  */
 package Gestor;
 
+import Entidades.Persona;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  *
  * @author edria
  */
 public class PersonaControl {
     DaoPersonaClase dao = new DaoPersonaClase();
-    //Hacer Agenda
+    Collection c = new ArrayList<>();
+
+    public Collection<Persona> cogerPorNombre(String nombre){
+        c = dao.findByName(nombre);
+        return c;
+    }
+    public Collection<Persona> cogerPorDNI(String dni){
+        c = dao.findByDNI(dni);
+        return c;
+    }
+    public Collection<Persona> cogerPorApellido(String apellido){
+        c = dao.findBySurname(apellido);
+        return c;
+    }
+    public Collection<Persona> cogerPorTelefono(Integer telefono){
+        c = dao.findByPhone(telefono);
+        return c;
+    }
+    public Collection<Persona> devolverTodo() throws SQLException {
+        c = dao.findAll();
+        return c;
+    }
+    public Persona cogerPorId(Long id){
+        Persona p = dao.findById(id);
+        return p;
+    }
+    public void insertar(String nombre, String apellido, String dni, Integer telefono, Long id){
+    }
     
+    public static void main(String args []) throws SQLException{
+        DaoPersonaClase dao = new DaoPersonaClase();
+        dao.contactar();
+        
+    }
 }
